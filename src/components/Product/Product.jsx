2b50bc5090a361a6216f0e9efa773be235e4bf65
dmmonir2003 <dmmonir2003@gmail.com>
2023-06-14 13:47:12 +0600

    daploy the site in surge

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,17 +1,29 @@
 import React from 'react';
-import './Product.css'
+import './Product.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 const Product = (props) => {
     const { name, img, price, seller, ratings } = props.product;
+    console.log(img);
+    const productAddToCart = props.productAddToCart;
+    const handleImageError = (event) => {
+        event.target.style.display = 'none';
+    };
     return (
         <div className='product'>
-            <img src={img} alt="" />
+            <img src={img} alt="Out Of Sell Not Stock Avilable"
+                className='img-alt' />
+
             <div className="product-info">
                 <h6 className='product-name'>{name}</h6>
                 <p>Manufacturer:${price}</p>
                 <p>Manufacturer: {seller}</p>
                 <p>Rating: {ratings} Star</p>
             </div>
-            <button className='btn-cart'>Add to Cart</button>
+            <button onClick={() => productAddToCart(props.product)} className='btn-cart'>
+                Add to Cart
+                <FontAwesomeIcon icon={faShoppingCart} />
+            </button>
         </div>
     );
 };
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,14 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import './Shop.css'
 import Product from '../Product/Product';
+
 const Shop = () => {
 
     const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([]);
+
     useEffect(() => {
         fetch('products.json')
             .then(res => res.json())
             .then(data => setProducts(data))
-    }, [])
+    }, []);
+    const productAddToCart = (product) => {
+        const newCart = [...cart, product];
+        setCart(newCart);
+    }
     return (
         <div className='shop-container'>
             <div className='product-container'>
@@ -16,11 +23,13 @@ const Shop = () => {
                     products.map(product => <Product
                         key={product.id}
                         product={product}
+                        productAddToCart={productAddToCart}
                     ></Product>)
                 }
             </div>
             <div className='order-summary-container'>
                 <h4>Order Summary</h4>
+                <h6>Selected Items: {cart.length}</h6>
             </div>
         </div>
     );
